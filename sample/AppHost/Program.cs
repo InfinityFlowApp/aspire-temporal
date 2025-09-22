76449e8ab8c3cc376a34392d29396c32c1952629
Matthew Austew <omnideth@gmail.com>
2025-09-22 13:25:22 -0500

    This is the minimum required code to get the ports to tie from the display to the resource.
    If you try to overwrite the original line 5 var temporal with ports, the dashboard has them but they do not connect.

--- sample/AppHost/Program.cs
+++ sample/AppHost/Program.cs
@@ -9,10 +9,21 @@ var temporal = await builder.AddTemporalServerContainer("temporal", x => x
     .WithDynamicConfigValue("frontend.enableUpdateWorkflowExecution", true)
 );
 
+var temporalWithPorts = await builder.AddTemporalServerContainer("temporalWithPorts", x => x
+    .WithPort(12345)
+    .WithUiPort(23456)
+    .WithLogFormat(LogFormat.Json)
+    .WithLogLevel(LogLevel.Info)
+    .WithNamespace("test1", "test2")
+    .WithDynamicConfigValue("frontend.enableUpdateWorkflowExecution", true)
+);
+
 builder.AddProject<Projects.Api>("api")
     .WithReference(temporal);
 
 builder.AddProject<Projects.Worker>("worker")
     .WithReference(temporal);
+
+
 
 builder.Build().Run();
