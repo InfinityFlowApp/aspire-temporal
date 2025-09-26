844ca9af849f546b360303f5cf9b0fce2819e00c
Elan Hasson <234704+ElanHasson@users.noreply.github.com>
2025-09-26 15:26:44 -0400

    Make AddTemporalServerContainer methods synchronous (#211)
    Removed asynchronous behavior from AddTemporalServerContainer
    methods across Program.cs and TemporalServerContainerBuilderExtensions.cs.
    Updated method signatures to return IResourceBuilder directly
    instead of Task<IResourceBuilder>. Simplified API usage by
    eliminating the need for async/await handling when adding
    Temporal server containers.

--- sample/AppHost/Program.cs
+++ sample/AppHost/Program.cs
@@ -2,14 +2,14 @@ using InfinityFlow.Aspire.Temporal;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var temporal = await builder.AddTemporalServerContainer("temporal", x => x
+var temporal = builder.AddTemporalServerContainer("temporal", x => x
     .WithLogFormat(LogFormat.Json)
     .WithLogLevel(LogLevel.Info)
     .WithNamespace("test1", "test2")
     .WithDynamicConfigValue("frontend.enableUpdateWorkflowExecution", true)
 );
 
-var temporalWithPorts = await builder.AddTemporalServerContainer("temporalWithPorts", x => x
+var temporalWithPorts = builder.AddTemporalServerContainer("temporalWithPorts", x => x
     .WithPort(12345)
     .WithUiPort(23456)
     .WithLogFormat(LogFormat.Json)
